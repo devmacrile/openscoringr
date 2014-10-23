330795e884e53c383517cd684b4c625c123b0907
U-VAL\RileyD <RileyD@RILEYD-L.val.vlss.local>
2014-10-23 16:03:02 -0400

    Added deploy.r

---
+++ R/deploy.r
@@ -0,0 +1,16 @@
+#' Deploy a model to the openscoring server
+#'
+#' This function takes a given model implemented in R,
+#' creates an appropriate PMML file to deploy to the server,
+#' and deploys this model to the openscoring server to be
+#' invoked via the REST API.
+#'
+#' @param model_name the name of the model to be deployed
+#' @export
+deploy <- function(model_name){
+
+
+
+
+
+}
