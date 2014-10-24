baea8a7b17a67ef6dcb83c08e5ef0c690fd76d0e
U-VAL\RileyD <RileyD@RILEYD-L.val.vlss.local>
2014-10-24 00:24:24 -0400

    Updated score, good news on the jsonlite functions

--- R/score.r
+++ R/score.r
@@ -3,22 +3,42 @@
 #' Takes feature data and returns a prediction response by applying
 #' the selected model to the data provided.  This can be called
 #' in either "single prediction" mode (i.e. one observation of features,
-#' one set of prediction values returned)
+#' one set of prediction values returned).
+#'
 #' @param model_name name of the model used to score
 #' @param host address of openscoring server
-#' @param data data on which to execute model
+#' @param data data on which to execute model.  Must be a data.frame and
+#' the colnames must match the activeFields of model_name.
+#' Run getModelSchema(model_name)$activeFields to get the active
 #' @param mode either 'single' or 'batch'
 #' @keywords score batch
 #' @export
-score <- function(model_name, host, data, mode){
+score <- function(model_name, host, data, mode="single"){
   if(!isDeployed(model_name, host)){
     stop(paste("No deployed model named ", model_name, sep=""))
   }
   if(class(data) != "data.frame"){
-    stop(paste("The 'data' parameter must have class data.frame
-               (class ", class(data), "provided", sep=""))
+    stop(paste("The 'data' parameter must have class 'data.frame'\n",
+               "(class '", class(data), "' provided)", sep=""))
   }
-  #Check data against model schema
+
+  schema <- getModelSchema(model_name, host)
+  active_fields <- unlist(schema$activeFields)
+  clnms <- colnames(data)
+  if(!setequal(clnms, active_fields)){
+    stop(paste("The 'data' field names do not match the activeFields of the model!"))
+  }
+
+  id <-
+  data <-
+  if(nrow(data) == 1){
+    # Single prediction
+    request <- toJSON()
+  }
+  else{
+    # Batch prediction
+
+  }
 
 
 
