fd5bb5a4152c98bbc90de540d39562964f8e342b
U-VAL\RileyD <RileyD@RILEYD-L.val.vlss.local>
2014-10-23 22:03:33 -0400

    Quick update

---
+++ R/getModelSchema.r
@@ -0,0 +1,15 @@
+#' Returns the schema of the given model
+#'
+#' The schema of the model includes "activeFields", "groupFields",
+#' "targetFields", and "outputFields".  Returns a list with this information
+#' associated with the model name.
+#'
+#' @param model_name the name of the model for which to retrieve schema
+#' @keywords schema model get
+#' @export
+getModelSchema <- function(model_name, host){
+
+
+
+
+}
--- R/score.r
+++ R/score.r
@@ -4,3 +4,15 @@
 #' the selected model to the data provided.  This can be called
 #' in either "single prediction" mode (i.e. one observation of features,
 #' one set of prediction values returned)
+#' @param model_name name of the model used to score
+#' @param host address of openscoring server
+#' @param d data on which to execute model
+#' @param mode either 'single' or 'batch'
+#' @keywords score batch
+#' @export
+#' @example
+score <- function(model_name, host, d){
+
+
+
+}
