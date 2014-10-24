400a52c3764acbb76743cc3a89759d35703e611b
U-VAL\RileyD <RileyD@RILEYD-L.val.vlss.local>
2014-10-23 21:44:17 -0400

    Added URL to DESC; added score.r

---
+++ R/score.r
@@ -0,0 +1,6 @@
+#' Score a deployed model on new data
+#'
+#' Takes feature data and returns a prediction response by applying
+#' the selected model to the data provided.  This can be called
+#' in either "single prediction" mode (i.e. one observation of features,
+#' one set of prediction values returned)
