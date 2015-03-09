8e2a06a6b5664b136383f701b77d8765c7fb1a22
Devin Riley <rileyde@umich.edu>
2015-03-08 22:41:04 -0400

    test-deploy.R

--- tests/testthat/test-deploy.R
+++ tests/testthat/test-deploy.R
@@ -1,9 +1,14 @@
+library(openscoringr)
 context("deploy")
 
+# Test prerequisites/constants
 OPENSCORING.SERVER <- "http://localhost:8080/openscoring"
 load("resources/iris-rf.Rdata")  # Loads iris.rf randomForest object
 
-deploy(iris.rf, "iris-rf", OPENSCORING.SERVER)
+
+test_that("model can be deployed to openscoring server", {
+	deploy(iris.rf, "iris-rf", OPENSCORING.SERVER)
+})
 
 
 
